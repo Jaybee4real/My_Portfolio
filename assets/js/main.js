622b7f614bdfc4c49784fc31f514758759ba071f
jaybee4real <okorojijaybee1@gmail.com>
2020-06-21 22:35:45 +0100

    _heading section design

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,25 @@ document.querySelector(".mobile-nav-toggle").addEventListener("click" ,() => {
     document.querySelector(".mobile-menu").classList.toggle("active")
 })
 
+
+
+// new Scene({
+//     ".introduction p": Scene.typing({
+//       text: `Hey! <span class="hand">&#128075 </span> Im`,
+//       duration: 3,
+//       delay: 3,
+//     }),
+//     ".cursor": {
+//       0: { opacity: 0 },
+//       0.5: { opacity: 0 },
+//       "0.5>": { opacity: 1 },
+//       1: { opacity: 1 },
+//       options: {
+//         iterationCount: 8,
+//       }
+//     },
+//   }, {
+//     iterationCount: "infinite",
+//     direction: "alternate",
+//     selector: true,
+//   }).play();
